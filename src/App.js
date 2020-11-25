02b13de0f6ab7f1202f08105795bde26228f71b9
tam <61011180@kmitl.ac.th>
2020-11-25 23:01:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
+import SignIn from './SignIn';
+import {BrowserRouter,Switch, Route}from 'react-router-dom';
+import SignUp from './Signup';
+import Page from './page';
+import Home from './pages';
+import Chat from './Chat';
+
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+   <div className="App">
+<Switch>
+  <Route path="/" component={Home} exact/>
+  <Route path="/signup" component={SignUp} exact/>
+  <Route path="/signin" component={SignIn} exact/>
+  <Route path="/page" component={Page} exact/>
+  <Route path="/chat" component={Chat} exact/>
+ </Switch>
+   
+
+
     </div>
+    
+    </BrowserRouter>
+    
   );
 }
 
